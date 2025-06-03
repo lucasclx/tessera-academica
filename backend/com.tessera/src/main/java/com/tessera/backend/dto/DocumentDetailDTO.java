62f5b83ae63c7e50d633cee69a1777ff7ca865da
Lucas Barbosa Calixto <81984351+lucasclx@users.noreply.github.com>
2025-06-03 19:18:48 -0300

    chore: remove unused imports

--- backend/com.tessera/src/main/java/com/tessera/backend/dto/DocumentDetailDTO.java
+++ backend/com.tessera/src/main/java/com/tessera/backend/dto/DocumentDetailDTO.java
@@ -1,12 +1,10 @@
 package com.tessera.backend.dto;
 
-import com.tessera.backend.entity.DocumentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
